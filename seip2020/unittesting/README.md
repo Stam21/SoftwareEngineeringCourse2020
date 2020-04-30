673b0fdf4206577bcae517e6e142abf27269e6a7
Stam21 <george.stamatopoulos1998@gmail.com>
2020-04-29 23:15:20 -0700

    Resolve issues before merging

--- seip2020/unittesting/README.md
+++ seip2020/unittesting/README.md
@@ -4,3 +4,12 @@
 ## Description
 
 This is a maven module. The purpose of this module is to perform unit tests in java. Some integer and array operations are tested but also a method that reads files.
+
+## Code Coverage
+
+In order to generate the code coverage report you need to run : 
+
+```
+mvn package jacoco:report
+```
+After executing the command you can find the code coverage report in the subfolder "target/site/jacoco".
--- seip2020/unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ seip2020/unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -1,69 +1,75 @@
 package math;
 
 /**
- * ArrayOperrationsTest purpose is to test ArrayOperrations class.
+ * ArithmeticOperationsTest purpose is to test ArithmeticOperations class.
  * 
  * @author George Stamatopoulos
  * @version 1.0
- * @since 2019-04-26
+ * @since 2020-04-23
  */
 
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
 import org.junit.Assert;
 import org.junit.Rule;
-
-import static org.mockito.Mockito.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ArithmeticOperationsTest {
+
 	ArithmeticOperations aop = new ArithmeticOperations();
 
-	// Create ArrayOperrations object using the objects that were created above.
 
-
-	/**
-	 * This is test_reverseArray method which tests if the signs of the integers
-	 * inside an array are reversed. It uses reverseSign method for each integer by
-	 * mocking IntegerOperations class.
-	 */
-
-	// Test if positive integers turn into negative.
+	// Test if division with 0 throws exception.
 	@Test(expected=ArithmeticException.class)
-	public void test_zero() {
+	public void test_divide_zero_denominator() {
 		aop.divide(3.0,0.0);
 	}
-
-	// Test if negative integers turn into positive.
+	
+	// Test if result of the division is correct when numerator is equal to 0.
 	@Test
-	public void test_correctness() {
+	public void test_divide_zero_numerator() {
+		Assert.assertEquals(0.0, aop.divide(0,3),0);
+	}
+
+	// Test if the result of the division is correct.
+	@Test
+	public void test_division_correctness() {
 		Assert.assertEquals(5.0, aop.divide(25,5),0);
 	}
 
 	// Define an expected exception for cases that throw exception.
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
-
+	
+	// Test that multiply with negative throws exception.
 	@Test
-	public void test_negative() {
+	public void test_multiply_negative() {
 
 		exception.expect(IllegalArgumentException.class);
 
 		aop.multiply(-4,-2);
 	}
-
+	
+	// Test if the result of the multiplication is correct.
 	@Test
-	public void test_multiply() {
+	public void test_multiply_correctness() {
 
 		Assert.assertEquals(25, aop.multiply(5,5));
 	}
-
+	
+	// Test that it throws exception when the result of multiplication is greater than the maximum integer.
 	@Test
-	public void test_out_of_bounds() {
+	public void test__multiply_out_of_bounds() {
 
 		exception.expect(IllegalArgumentException.class);
 		aop.multiply(Integer.MAX_VALUE/2,3);
+	}
+	
+	// Test that it returns correct result when y is equal to 0.
+	@Test
+	public void test_multiply_zero_y() {
+
+		Assert.assertEquals(0, aop.multiply(5,0));
 	}
 } 
